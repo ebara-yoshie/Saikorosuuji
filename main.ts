bb7972d0b93c7aed39567a92ad296866137eca71
ebara-yoshie <111729825+ebara-yoshie@users.noreply.github.com>
2022-08-22 16:41:36 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.forever(function () {
+	
+})
